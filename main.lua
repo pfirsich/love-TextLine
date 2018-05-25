c74006ee4256433b92756438ee521b0b0d78c9c3
Joel Schumacher <joelschum@gmail.com>
2018-05-25 15:17:32 +0200

    Take out _focused of out fhe textLine object in the example to make it clearer I am not modifying some magical internal state, but just handling the focusing myself

--- main.lua
+++ main.lua
@@ -3,7 +3,7 @@ local TextLine = require("textline")
 function love.load()
     local font = love.graphics.newFont(20)
     textLine = TextLine(font, 20, 20, 300, 30)
-    textLine._focused = true
+    textLineFocused = true
     love.keyboard.setKeyRepeat(true)
 end
 
@@ -11,36 +11,36 @@ function love.draw()
     love.graphics.setColor({1.0, 1.0, 1.0})
     love.graphics.rectangle("line", textLine.x - 5, textLine.y - 5,
         textLine.width + 10, textLine.height + 10)
-    textLine:draw({0.5, 0.5, 0.5}, textLine._focused)
+    textLine:draw({0.5, 0.5, 0.5}, textLineFocused)
 end
 
 function love.keypressed(...)
-    if textLine._focused then
+    if textLineFocused then
         textLine:keyPressed(...)
     end
 end
 
 function love.textinput(...)
-    if textLine._focused then
+    if textLineFocused then
         textLine:textInput(...)
     end
 end
 
 function love.mousepressed(x, y, button)
-    textLine._focused = textLine:inArea(x, y, 5)
-    if textLine._focused and button == 1 then
+    textLineFocused = textLine:inArea(x, y, 5)
+    if textLineFocused and button == 1 then
         textLine:mousePressed(x, y)
     end
 end
 
 function love.mousemoved(x, y, dx, dy)
-    if textLine._focused then
+    if textLineFocused then
         textLine:mouseMoved(x, y)
     end
 end
 
 function love.mousereleased(x, y, button)
-    if textLine._focused and button == 1 then
+    if textLineFocused and button == 1 then
         textLine:mouseReleased(x, y)
     end
 end
